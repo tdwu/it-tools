62d872f21f87e09022f5dd0c2989380e39c46d9e
tongding.wu <251795891@qq.com>
2024-10-11 03:40:08 +0800

    跳转bug修复

--- src/tools/open-iframe/open-iframe.vue
+++ src/tools/open-iframe/open-iframe.vue
@@ -1,9 +1,38 @@
 <script setup lang="ts">  
  
-import { onMounted,onUnmounted } from 'vue'
+import { useRouter } from 'vue-router'
+import { onMounted,onUnmounted,ref } from 'vue'
+const route = useRoute()
+const url = ref("/")
+
+const router = useRouter()
+watch(() => router.currentRoute.value.path,
+  (toPath) => {
+    const currentRoute = router.currentRoute.value
+    //要执行的方法
+    //query
+    let qUrl = currentRoute.meta.query.url; 
+    //params
+    let pUrl = currentRoute.meta.params.url;
+    url.value = pUrl
+
+    let targetName = currentRoute.path;
+    console.log("参数1", qUrl, pUrl, targetName, currentRoute)    
+  }
+  ,{immediate: true,deep: true}
+)
 
 onMounted(() => {
-  document.querySelector('.tool-layout').style.display = 'none';
+  document.querySelector('.tool-layout').style.display = 'none'; 
+
+  // //query
+  // let qUrl = route.meta.query.url; 
+  // //params
+  // let pUrl = route.meta.params.url;
+  // url.value = pUrl
+
+  // let targetName = route.path;
+  // console.log("参数2", qUrl, pUrl, targetName, route)    
 })
 
 onUnmounted(()=>{
@@ -11,21 +40,12 @@ onUnmounted(()=>{
 }) 
 
 
-//首先在setup中定义
-const route = useRoute()
-//query
-let qUrl = route.meta.query.url; 
-//params
-let pUrl = route.meta.params.url;
-
-let targetName = route.path;
-console.log("参数", qUrl, pUrl, targetName, route)    
 
 </script>
 
 <template>
   <div>
-    <iframe id="bi_iframe" :src="pUrl" style="width:calc(100vw - 240px);height:calc(100vh - 26px  - 26px - 42px - 10px);margin-top:10px;" frameborder="0" scrolling="auto"></iframe>
+    <iframe id="bi_iframe" :src="url" style="width:calc(100vw - 240px);height:calc(100vh - 26px  - 26px - 42px - 10px);margin-top:10px;" frameborder="0" scrolling="auto"></iframe>
   </div>
 </template>
 

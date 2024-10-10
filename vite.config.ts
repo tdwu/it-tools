313b461ca057a75218f8d244a0f1a0dc5f8155d5
tongding.wu <251795891@qq.com>
2024-10-10 21:12:54 +0800

    打包

--- vite.config.ts
+++ vite.config.ts
@@ -15,11 +15,15 @@ import { VitePWA } from 'vite-plugin-pwa';
 import markdown from 'vite-plugin-vue-markdown';
 import svgLoader from 'vite-svg-loader';
 import { configDefaults } from 'vitest/config';
+import viteCompression from 'vite-plugin-compression'
 
-const baseUrl = process.env.BASE_URL ?? '/';
+const baseUrl = process.env.BASE_URL ?? '/it/';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig({ 
+  server:{
+    host: '0.0.0.0',
+  },
   plugins: [
     VueI18n({
       runtimeOnly: true,
@@ -67,22 +71,22 @@ export default defineConfig({
         background_color: '#f1f5f9',
         icons: [
           {
-            src: '/favicon-16x16.png',
+            src: baseUrl + '/favicon-16x16.png',
             type: 'image/png',
             sizes: '16x16',
           },
           {
-            src: '/favicon-32x32.png',
+            src: baseUrl + '/favicon-32x32.png',
             type: 'image/png',
             sizes: '32x32',
           },
           {
-            src: '/android-chrome-192x192.png',
+            src: baseUrl + '/android-chrome-192x192.png',
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: '/android-chrome-512x512.png',
+            src: baseUrl + '/android-chrome-512x512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable',
@@ -97,6 +101,10 @@ export default defineConfig({
       resolvers: [NaiveUiResolver(), IconsResolver({ prefix: 'icon' })],
     }),
     Unocss(),
+    viteCompression({ 
+      threshold: 8192, // 对大于 1mb 的文件进行压缩
+      verbose: true
+    })
   ],
   base: baseUrl,
   resolve: {
